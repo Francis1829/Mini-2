99a049188cf69339498c5bd9e473eeb861ca7cf5
Edward <140621113+EddyingPrime@users.noreply.github.com>
2023-10-12 14:50:18 +0800

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { useState } from 'react'
-import './App.css'
-import Navbar from './components/Navbar'
+import { useState } from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-    <Navbar />
+      <Navbar />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
