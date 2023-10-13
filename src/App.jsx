d3130da05af3344b65e353663f6fb2fbf7dc54ad
Francis Mar Subsilica <143585435+Francis1829@users.noreply.github.com>
2023-10-13 15:38:24 -0400

    navbar
    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
-import { useState } from 'react'
+import React from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
+import Home from './components/Pages/Home'
+import Category from './components/Pages/Category'
+import Games from './components/Pages/Games'
+import Contact from './components/Pages/Contact'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
 
   return (
     <>
+    <Router>
     <Navbar />
+      <Routes>
+        <Route path="/Home" element={<Home />} />
+        <Route path="/Category" element={<Category />} />
+        <Route path="/Games" element={<Games />} />
+        <Route path="/Contact" element={<Contact />} />
+      </Routes>
+    </Router>
     </>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,36 +1,45 @@
-import React from 'react'
 import Logo from './img/Logo1.png'
+import React from 'react'
 import { Link } from  "react-router-dom";
 
 export default function Navbar() {
-    // const menuList = [
-    //     { 
-    //     name: "Home",
-    //     link:  "/",
-    // },
-    // {
-    //    name: "Categories",
-    //    link: "/",
-    // },
-    // { 
-    //     name: "Games",
-    //     link:  "/",
-    // },
-    // { 
-    //     name: "Cafes",
-    //     link:  "/",
-    // },     
-    // ]
-
+  const menuList = [
+    { 
+    name: "Home",
+    link:  "/Home",
+},
+{ 
+    name: "Games",
+    link:  "/Games",
+},
+{ 
+    name: "Contact",
+    link:  "/Contact",
+},    
+{ 
+  name: "Category",
+  link:  "/Category",
+},   
+]
   return (
     <>
-    <header>
+    <header className="sticky top-0">
       <nav className='p-2 px-[5rem]'>
-        <div className="navbar flex justify-start gap-10 text-white ">
-          <div className="logo flex items-center text-[25px] uppercase"><img src={Logo} alt="" width={35} className="mr-1 md:cursor-pointer"/>ocialzone</div>
-          <div className="navList flex justify-center items-center gap-4 text-[16px] font-[300]">
-             home
-            
+        <div className="navbar flex justify-between text-white ">
+          <div className="flex justify-start gap-16">
+          <Link to="/Home">
+          <div className="logo flex items-center text-[25px] uppercase"><img src={Logo} alt="" width={35} className="mr-1 md:cursor-pointer"/>ocializone</div>
+          </Link>
+          <div className="navList md:flex hidden justify-center items-center gap-8">
+           {menuList.map((item, idx) => (
+                <Link className="hover:text-[#000] ease-in-out duration-300" key={idx} to={item.link}>
+                  {item.name}
+                </Link>
+              ))}
+            </div>
+            </div>
+            <div className="btnn w-[185px] flex justify-center">
+            <button className="bg-theme-color rounded-full py-2 px-9">Get Started</button>
             </div>
         </div>
       </nav>
