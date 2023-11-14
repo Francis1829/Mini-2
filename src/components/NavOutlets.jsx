6793c23a6cac2c83af3ea4d76e4c58ffbbf4885d
Francis Mar Subsilica <143585435+Francis1829@users.noreply.github.com>
2023-11-15 00:32:03 +0900

    maps

--- src/components/NavOutlets.jsx
+++ src/components/NavOutlets.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from './Navbar'
 import Footer from './Footer'
-import Covid from './covid'
+import Covid from '../components/Covid'
 import { Outlet } from 'react-router-dom'
 
 function NavOutlets() {
