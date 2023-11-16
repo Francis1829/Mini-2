37b8203157726cfc040ff27f5444560c36ae89a9
Francis Mar Subsilica <143585435+Francis1829@users.noreply.github.com>
2023-11-16 22:48:36 +0900

    responsive

--- src/components/Pages/Gallery.jsx
+++ src/components/Pages/Gallery.jsx
@@ -1,46 +1,46 @@
-import React, { useEffect, useState } from 'react';
-import fetch from 'node-fetch';
-import { createClient } from 'pexels';
+// import React, { useEffect, useState } from 'react';
+// import { createClient } from 'pexels';
 
-function Gallery() {
-  const [photos, setPhotos] = useState([]);
-  const client = createClient('LsfzoEK6GYaDT9mchfSS7vLGMyiuumByYueS5S8Fz6swrVlZYjcJvDoA'); 
-  const query = 'Gaming pc';
+// function Gallery() {
 
-  useEffect(() => {
-    client.photos.search({ query, per_page: 50 })
-      .then(response => {
-        setPhotos(response.photos);
-      })
-      .catch(error => {
-        console.error('Error fetching photos:', error);
-      });
-  }, []);
+//   const [photos, setPhotos] = useState([]);
+//   const client = createClient('LsfzoEK6GYaDT9mchfSS7vLGMyiuumByYueS5S8Fz6swrVlZYjcJvDoA'); 
+//   const query = 'Gaming pc';
 
-  return (
-    <> <div className="flex w-full">
-    <div className="w-full p-10">
-      <div className="font-semibold lg:text-[3rem] text-[2rem] font-[Agency] p-10 lg:text-start text-center">Gallery</div>
-      <div className="shop-body flex flex-wrap justify-center items-center">
-      {photos.map((photo) => (
-        <>
-      <div key={photos.id} className='flex flex-col box-border cursor-pointer  m-2 my-4'>
-        <div className="img flex justify-center items-center">
-      <img src={photo.src.medium} alt={photo.photographer} className='rounded-md' />
-      </div>
-      <div className="flex flex-col p-2">
-      <span className='text-[20px] font-semibold font-[Sudo] lg:text-start text-center'>Photographer: {photo.photographer}</span>
+//   useEffect(() => {
+//     client.photos.search({ query, per_page: 50 })
+//       .then(response => {
+//         setPhotos(response.photos);
+//       })
+//       .catch(error => {
+//         console.error('Error fetching photos:', error);
+//       });
+//   }, []);
+
+//   return (
+//     <> <div className="flex w-full">
+//     <div className="w-full p-10">
+//       <div className="font-semibold lg:text-[3rem] text-[2rem] font-[Agency] p-10 lg:text-start text-center">Gallery</div>
+//       <div className="shop-body flex flex-wrap justify-center items-center">
+//       {photos.map((photo) => (
+//         <>
+//       <div key={photos.id} className='flex flex-col box-border cursor-pointer  m-2 my-4'>
+//         <div className="img flex justify-center items-center">
+//       <img src={photo.src.medium} alt={photo.photographer} className='rounded-md' />
+//       </div>
+//       <div className="flex flex-col p-2">
+//       <span className='text-[20px] font-semibold font-[Sudo] lg:text-start text-center'>Photographer: {photo.photographer}</span>
       
-      </div>
-      </div>
-      </>
-    ))}
-      </div>
-    </div>
-  </div>
-    </>
+//       </div>
+//       </div>
+//       </>
+//     ))}
+//       </div>
+//     </div>
+//   </div>
+//     </>
 
-  );
-}
+//   );
+// }
 
-export default Gallery;
+// export default Gallery;
